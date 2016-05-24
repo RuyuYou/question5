90cbf4eb7e5ea228493467548ecc44e5cc87b12c
Ruyu You <727809166@qq.com>
2016-05-24 11:53:01 +0800

    finish double_dimensional_array_to_one_2.js

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,36 @@
-'use strict';
-
 function double_to_one(collection) {
+  var oneDimensionalArray = buildOneDimensionalArray(collection);
+  var reNewArray = buildReNewArray(oneDimensionalArray);
+  return reNewArray;
+}
 
-  //在这里写入代码
+function buildOneDimensionalArray(collection) {
+  var oneDimensionalArray = [];
+  collection.forEach(function (element) {
+    for (var i = 0; i < element.length; i++) {
+      oneDimensionalArray.push(element[i]);
+    }
+  });
+  return oneDimensionalArray;
+}
+
+function findReNewArray(element, reNewArray) {
+  for (var i = 0; i < reNewArray.length; i++) {
+    if (element === reNewArray[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function buildReNewArray(oneDimensionArray) {
+  var reNewArray = [];
+  for (var i = 0; i < oneDimensionArray.length; i++) {
+    if (findReNewArray(oneDimensionArray[i], reNewArray)) {
+      reNewArray.push(oneDimensionArray[i]);
+    }
+  }
+  return reNewArray;
 }
 
 module.exports = double_to_one;
