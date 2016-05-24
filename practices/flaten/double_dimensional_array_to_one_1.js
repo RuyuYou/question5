0b3c1e376f6c64a53ff4b6eada99430a0bec648d
Ruyu You <727809166@qq.com>
2016-05-24 09:41:44 +0800

    add double_dimensional_array_to_one_1.js

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,20 @@
-'use strict';
-
 function double_to_one(collection) {
-
-  //在这里写入代码
+  var oneDimensionalArray = buildOneDimensionalArray(collection);
+  return oneDimensionalArray;
 }
+
+function buildOneDimensionalArray(collection) {
+  var oneDimensionalArray = [];
+  collection.forEach(function (element) {
+    if (element.length === undefined) {
+      oneDimensionalArray.push(element);
+    }
+    for (var i = 0; i < element.length; i++) {
+      oneDimensionalArray.push(element[i]);
+    }
+  });
+  return oneDimensionalArray;
+}
+
 
 module.exports = double_to_one;
